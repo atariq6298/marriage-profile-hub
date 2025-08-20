d49a2a8f5c0ff1ab0b0a65c6df9fc58a68de5cd9
Ali Tariq <atariq.sharafi@gmail.com>
2025-08-20 15:43:15 +0500

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
-from app import db, bcrypt
+from flask_app import db, bcrypt
 from models import User, Profile
 
 api_bp = Blueprint('api', __name__)
